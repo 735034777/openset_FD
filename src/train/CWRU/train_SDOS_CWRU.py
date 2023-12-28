9f31fb1af98b323c812bc12c8569e64667ad8c26
dell-tsinghua <735034777@qq.com>
2023-12-28 17:14:48 +0000

    加入lmnn距离计算

--- src/train/CWRU/train_SDOS_CWRU.py
+++ src/train/CWRU/train_SDOS_CWRU.py
@@ -13,27 +13,33 @@ SAVE_PATH = r"H:\project\imbalanced_data\openset_FD\data"
 
 
 def train_SDOS_CWRU(result_path):
+
     results = pd.read_csv(result_path)
+    metric_types_list = ["cosine","lmnn","euclidean","manhattan"]
     for i in range(60):
         trainlabels,testlabels = save_SDOS_dataset(SAVE_PATH,FOLDER_PATH)
 
         openness = calculate_openness(len(trainlabels), len(set(trainlabels+testlabels)))
         accuracy = train_base_model()
-        testaccuracy,precision,recall,f1,youdens_index = calculate_openmax_accuracy(5)
-        open_fault = set(testlabels)-set(trainlabels)
-        new_row = [trainlabels,testlabels,open_fault,openness,accuracy,testaccuracy,precision,recall,f1,youdens_index]
-        # 确定新行的索引位置
-        new_index = len(results)
+        for metric_types in metric_types_list:
+            for i in range(1,5):
+                testaccuracy,precision,recall,f1,youdens_index,soft_accuracy,soft_metrix = calculate_openmax_accuracy(metric_types,5*i)
+                open_fault = set(testlabels)-set(trainlabels)
+                new_row = [trainlabels,testlabels,metric_types,5*i,open_fault,openness,accuracy,testaccuracy,precision,
+                           recall,f1,youdens_index,soft_accuracy,soft_metrix]
+                print("metric is {},tail size is {}, accuracy is {.5f}".format(metric_types,5*i,testaccuracy))
+                # 确定新行的索引位置
+                new_index = len(results)
 
-        # 扩展DataFrame
-        results = results.reindex(results.index.tolist() + [new_index])
+                # 扩展DataFrame
+                results = results.reindex(results.index.tolist() + [new_index])
 
-        # 使用iloc填充新行
-        results.iloc[new_index] = new_row
+                # 使用iloc填充新行
+                results.iloc[new_index] = new_row
         # results.append(dict(zip(results.columns,new_row)),ignore_index=True)
         results.to_csv(result_path)
 
 if __name__=="__main__":
-    result = "SDOS_RESULTS_highDimSpace.csv"
+    result = "SDOS_RESULTS_DIFF_METRIC.csv"
     train_SDOS_CWRU(result)
 
